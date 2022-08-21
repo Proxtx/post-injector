8840e4834a5181f240d638249f0372b27e7dd323
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-08-21 22:41:15 +0200

    Update fileUpload.js

--- private/fileUpload.js
+++ private/fileUpload.js
@@ -1,6 +1,6 @@
 import { auth } from "../public/meta.js";
 import { getJob } from "../public/jobManagement.js";
-import { extname } from "path";
+import { extname, resolve } from "path";
 import fs from "fs/promises";
 
 export const uploadHandler = async (req, res) => {
@@ -13,6 +13,6 @@ export const uploadHandler = async (req, res) => {
   let ext = extname(file.name);
   let filePath = job.path + "/" + job.data.downloads.length + ext;
   await fs.writeFile(filePath, file.data);
-  job.data.downloads.push(filePath);
+  job.data.downloads.push(resolve(filePath));
   res.status(200).send();
 };
